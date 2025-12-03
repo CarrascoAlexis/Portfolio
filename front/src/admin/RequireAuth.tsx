cd43a8ebeb9169155fe5fa0d0cb50b965d077c90
kroko_san <acarrasco@gaming.tech>
2025-12-03 13:47:37 +0100

    fix(front): separate PropsWithChildren import for clarity in RequireAuth component

--- front/src/admin/RequireAuth.tsx
+++ front/src/admin/RequireAuth.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState, PropsWithChildren } from 'react';
+import { useEffect, useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
 export default function RequireAuth({ children }: PropsWithChildren) {
